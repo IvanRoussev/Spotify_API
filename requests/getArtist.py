65aebc8b1869a423187d03b41934e25e4393743a
Ivan Roussev <ivan.roussev12@gmail.com>
2023-05-03 12:01:08 -0700

    parsed through json to get data

--- requests/getArtist.py
+++ requests/getArtist.py
@@ -1,6 +1,7 @@
 from header import ACCESS_TOKEN
 
 import requests
+import json
 
 
 artist_url = "https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg"
@@ -8,11 +9,24 @@ artist_url = "https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg"
 HEADERS = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
 
 
-def get_artists(url, headers):
+def fetch_data(url, headers):
     # endpoint https://api.spotify.com/v1/artists/{id}
 
     response = requests.get(url, headers=headers)
     return response.content
 
 
-print(get_artists(artist_url, HEADERS))
+def get_artist_information(data):
+    data = json.loads(data)
+    print(data)
+    name = data["name"]
+    followers = data["followers"]["total"]
+    popularity_rating = data["popularity"]
+    list_genres = data["genres"]
+    print(name)
+    print(followers)
+    print(popularity_rating)
+    print(list_genres)
+
+
+get_artist_information(fetch_data(artist_url, HEADERS))
---
+++ requests/getPlaylist.py
@@ -0,0 +1,2 @@
+# http GET 'https://api.spotify.com/v1/playlists/45qu5W1B8DYYdoVhoJ7woX?market=CA' \
+#  Authorization:'Bearer BQBoqY...QMkCrU'
